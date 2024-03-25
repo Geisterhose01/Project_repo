e4bab0aa11ac47f5819063ffd7e7608080a134e6
MrMolino <163356774+MrMolino@users.noreply.github.com>
2024-03-25 10:44:37 +0100

    Test2

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
 import streamlit as st
-
-st.write("Connected everything")
-
-<<<<<<< HEAD
-st.write("Hello")
-
-st.write("Goodbye")
-=======
-st.write("Hello World")
-<<<<<<< HEAD
-
->>>>>>> b81097b68feccf2e08cb1e045a0a3c7192ce8405
-=======
-st.write(input("Schreibe deinen Namen"))
->>>>>>> 841e7201b59dab74099083f054bd2e2d05fa612e
