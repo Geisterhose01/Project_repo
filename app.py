cb86adc35ef156b55eb6e63fd7b35ca46ad6d185
juliasgnr <162497246+juliasgnr@users.noreply.github.com>
2024-03-25 10:55:05 +0100

    list files

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.write('hello world')
 st.text_input('schreibe deine Namen')
+
+#Give me a list with all the files in the folder.streamlit
+
+# Path: streamlit/__init__.py
+import os
+
+def list_files():
+    return os.listdir()
+
+
