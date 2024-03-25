54b1c74790856194cda0c21bead1837e483b31a2
Geisterhose01 <163872560+Geisterhose01@users.noreply.github.com>
2024-03-25 11:02:16 +0100

    changes

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ import streamlit as st
 
 st.write('hello world')
 st.text_input('schreibe deine Namen')
+<<<<<<< HEAD
+
+
+st.text_input('wie alt bist du?')
+=======
 
 st.write("apfel")
+>>>>>>> 6235fc6a1a67719fd7c1eac37bd02cd274d87a0c
