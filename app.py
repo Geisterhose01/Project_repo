2e24789cd45dbba950e99be340103322ae614d0d
Sunny22155 <162025151+Sunny22155@users.noreply.github.com>
2024-03-26 17:29:44 +0100

    button

--- app.py
+++ app.py
@@ -9,3 +9,18 @@ st.text_input('wie alt bist du?')
 st.write("apfel")
 
 st.text_input('woher kommst du?')
+
+st.title('Dateideen')
+st.header('Dateideen für den Sommer')
+st.write_steam('Hier sind ein paar Dateideen für den Sommer:')
+x = st.text_input('schreibe deine Namen')
+st.write(f'Dein Name ist: {x}')
+
+st.text_input('wie alt bist du?')
+
+st.text_input('woher kommst du?')
+st.button('Submit')
+If st.button('Submit'):
+    st.write('Danke für die Info')
+else:
+    st.write('Bitte fülle die Felder aus')
