ef1be25dcb2a9a7f3f618c27b5ed0ba18e1fb74d
Geisterhose01 <163872560+Geisterhose01@users.noreply.github.com>
2024-04-04 12:07:07 +0200

    changes

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ st.title('Datefinder')
 st.header('Finde deine passende Dateideen für den Sommer')
 st.subheader('Beginnen wir mit einigen Angaben von dir..')
 x = st.text_input('Wie heisst du?')
-st.write(f'Hallo {x}'', schön, dass du unsere App nutzt')
+st.write(f'Hallo {x}'', schön, dass du unsere App nutzt.')
 
 alter = st.slider ('Wie alt bist du?', 0, 100, 25)
 st.write('Ich bin', alter, 'Jahre alt.')
 
 
-st.subheader('Welche Kategorie möchtest du nutzen?')
+st.subheader('In welcher Kategorie möchtest du ein Date finden?')
 
 st.link_button('Events', 'https://projectrepo-c4ukv9mberd3dykzagyjdq.streamlit.app/events')
 st.link_button('Food', 'https://projectrepo-c4ukv9mberd3dykzagyjdq.streamlit.app/food')
