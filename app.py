bf4087ff48ce78b2a6070b1e4b1db876c4041024
Geisterhose01 <163872560+Geisterhose01@users.noreply.github.com>
2024-04-04 11:28:23 +0200

    trying some things out

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import pandas as pd
 import numpy as np
 
 
-st.title('Dateideen')
-st.header('Dateideen für den Sommer')
-st.subheader('Hier sind ein paar Dateideen für den Sommer:')
+st.title('Datefinder')
+st.header('Finde deine passende Dateideen für den Sommer')
+st.subheader('Beginnen wir mit einigen Angaben von dir..')
 x = st.text_input('Wie heisst du?')
 st.write(f'Dein Name ist: {x}')
 
 alter = st.slider ('Wie alt bist du?', 0, 100, 25)
 st.write('Ich bin', alter, 'Jahre alt')
+st.balloons()
+
+st.subheader('Hier sind ein paar Dateideen für den Sommer:')
 
 data = pd.read_csv('food.csv')
 st.write(data)
