cc2ba4c7e8518e74503e4a7ed6477361c28bf18e
Geisterhose01 <163872560+Geisterhose01@users.noreply.github.com>
2024-04-04 11:41:05 +0200

    ''

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.write(f'Dein Name ist: {x}')
 alter = st.slider ('Wie alt bist du?', 0, 100, 25)
 st.write('Ich bin', alter, 'Jahre alt')
 
-st.write(f'Hallo: {x}, schön, dass du unsere App nutzt')
+st.write(f'Hallo: {x}'', schön, dass du unsere App nutzt')
 
 st.subheader('Welche Kategorie möchtest du nutzen?')
 
