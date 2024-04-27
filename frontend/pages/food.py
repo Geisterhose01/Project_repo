48fb095235333dcb4608d2b5a86090eae8300106
juliasgnr <162497246+juliasgnr@users.noreply.github.com>
2024-04-27 13:33:28 +0200

    programmer

--- frontend/pages/food.py
+++ frontend/pages/food.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 import pandas as pd
-from ..backend.apiController import searchByIngredients
+from .backend.apiController import searchByIngredients
 
 
 #Titelseite machen
